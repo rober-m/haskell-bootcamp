6e2e4492f7622713352793d82dc8492b32c9eff1
Robertino <48034748+rober-m@users.noreply.github.com>
2022-05-23 15:35:43 -0300

    prepared file to explain how to do the homework (add more questions)

--- code/week01/src/MyLib.hs
+++ code/week01/src/MyLib.hs
@@ -1,4 +1,8 @@
-module MyLib (someFunc) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Write a function that takes a value and multiplies it by 3. Test that it works using GHCi.
+
+
+-- Write a function that calculates the area of a circle. Test that it works using GHCi.
+
+
+-- TODO: Write two more qustions
